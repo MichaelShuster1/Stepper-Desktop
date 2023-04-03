56b48b7b9ca06fba38fdc6dc1339feca84a09d26
Michael Shuster <michaelsu@mta.ac.il>
2023-04-03 12:11:43 +0300

    test3

--- UI/src/Main.java
+++ UI/src/Main.java
@@ -3,8 +3,8 @@ public class Main
 {
     public static void  main(String args[])
     {
-        System.out.println("Static block");
-        System.out.println("Static block");
+        System.out.println("hello world");
+        System.out.println("bla bla");
 
     }
 }
