ea8ace25418c30d65ff1b1f27095d85fe2e7a402
Michael Shuster <michaelsu@mta.ac.il>
2023-04-07 16:41:19 +0300

    added Flow class

--- Engine/src/Steps/CollectFiles.java
+++ Engine/src/Steps/CollectFiles.java
@@ -8,10 +8,13 @@ public class CollectFiles extends Step
     public CollectFiles(String name)
     {
         super(name,true);
+
         DataString dataString=new DataString("FOLDER_NAME");
         inputs.add(new Input(dataString,true,true));
+
         dataString=new DataString("FILTER");
         inputs.add(new Input(dataString,true,false));
+
         outputs.add(new Output(new DataList("FILES_LIST")));
         outputs.add(new Output(new DataNumber("TOTAL_FOUND")));
     }
--- UI/src/Main.java
+++ UI/src/Main.java
@@ -1,12 +1,9 @@
 import DataDefinitions.Input;
-import Steps.CollectFiles;
-import Steps.SpendSomeTime;
-import Steps.Step;
+import Steps.*;
+import javafx.util.Pair;
+import sun.java2d.loops.CustomComponent;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main
 {
@@ -20,12 +17,16 @@ public class Main
         List<Step> steps=new ArrayList<>();
         steps.add(new SpendSomeTime("s1"));
         steps.add(new CollectFiles("S2"));
+        steps.add(new FilesRenamer(("S3")));
+        steps.add(new CSVExporter("S4"));
 
-        Input input1=steps.get(0).getInput(0);
-        input1.setData(number);
+        Input input=steps.get(0).getInput(0);
+        input.setData(number);
         System.out.println("Start of delay: "+ new Date());
         steps.get(0).Run();
         System.out.println("End of delay: "+ new Date());
 
+
+        List<List<List<Pair<Integer,Integer>>>> connections =new ArrayList<>();
     }
 }
