ed4746d27bc3a79af9a9f211b9af7e9c7f44a32d
Igal Kaminski <igalka@mta.ac.il>
2023-04-27 11:56:24 +0300

    dto branch

---
+++ Engine/src/dto/FlowDefinitionDTO.java
@@ -0,0 +1,15 @@
+package dto;
+
+import java.util.Set;
+
+public class FlowDefinitionDTO {
+    String name;
+    String description;
+    Set<String> formal_outputs;
+    boolean readOnly;
+    //List<StepDTO>
+    //List<FreeInputsDTO>
+    //List<OutputsDTO>}
+}
+
+
