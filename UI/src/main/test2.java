0659c06732d45254187220d1471f7e2f5745c013
Igal Kaminski <igalka@mta.ac.il>
2023-05-18 22:02:14 +0300

    testing controllers

---
+++ UI/src/main/test2.java
@@ -0,0 +1,31 @@
+package main;
+
+import controllers.AppController;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.net.URL;
+
+public class test2 extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        URL resource =getClass().getResource("MainScreen.fxml");
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(resource);
+        Parent root = loader.load(resource.openStream());
+        AppController controller = loader.getController();
+
+        Scene scene = new Scene(root,850,600);
+        primaryStage.setTitle("Stepper");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
+}
