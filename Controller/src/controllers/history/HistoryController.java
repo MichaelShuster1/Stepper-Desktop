334bd619e1566de85c05dc305c44dbc1cef1f178
Igal Kaminski <igalka@mta.ac.il>
2023-05-20 19:56:33 +0300

    added filter to history

--- Controller/src/controllers/history/HistoryController.java
+++ Controller/src/controllers/history/HistoryController.java
@@ -4,6 +4,9 @@ import controllers.AppController;
 import dto.FlowExecutionDTO;
 import dto.StepExecutionDTO;
 import enginemanager.EngineApi;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
@@ -35,11 +38,14 @@ public class HistoryController {
 
     private TableView<FlowExecutionDTO> historyTableView;
 
+    private ObservableList<FlowExecutionDTO> tableData;
+
     private TableColumn<FlowExecutionDTO,String> flowNameColumnView;
 
     private TableColumn<FlowExecutionDTO,String> activationTimeColumnView;
 
     private TableColumn<FlowExecutionDTO,String> stateColumnView;
+
 
 
 
@@ -59,6 +65,11 @@ public class HistoryController {
         historyTableView.getColumns().forEach(column -> column.setMinWidth(200));
         historyTableView.setOnMouseClicked(e->rowClick(new ActionEvent()));
         stackTableView.getChildren().add(historyTableView);
+        stateFilterView.getItems().addAll("NONE", "SUCCESS", "WARNING", "FAILURE");
+        stateFilterView.setValue("NONE");
+        stateFilterView.setOnAction(event -> filterTable());
+        tableData = FXCollections.observableArrayList();
+        historyTableView.setItems(tableData);
     }
 
 
@@ -85,6 +96,37 @@ public class HistoryController {
 
     public void addRow(FlowExecutionDTO flowExecutionDTO)
     {
-        historyTableView.getItems().add(flowExecutionDTO);
+        tableData.add(flowExecutionDTO);
+    }
+
+    public void filterTable()
+    {
+        String choice = stateFilterView.getValue();
+        FilteredList<FlowExecutionDTO> filteredData = new FilteredList<>(tableData);
+        switch(choice) {
+            case "NONE":
+                historyTableView.setItems(tableData);
+                break;
+            case "SUCCESS":
+                filteredData.setPredicate(item -> {
+                    return item.getStateAfterRun().equals("SUCCESS");
+                });
+                historyTableView.setItems(filteredData);
+                break;
+            case "FAILURE":
+                filteredData.setPredicate(item -> {
+                    return item.getStateAfterRun().equals("FAILURE");
+                });
+                historyTableView.setItems(filteredData);
+                break;
+            case "WARNING":
+                filteredData.setPredicate(item -> {
+                    return item.getStateAfterRun().equals("WARNING");
+                });
+                historyTableView.setItems(filteredData);
+                break;
+
+        }
+        historyTableView.refresh();
     }
 }
